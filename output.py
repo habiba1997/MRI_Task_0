0b1e704688e4e8e8e7105b25647292c180d20afc
habiba1997 <habiba.hamad97@eng-st.cu.edu.eg>
2019-02-16 19:10:17 +0200

    Latest Design

--- output.py
+++ output.py
@@ -13,18 +13,21 @@ class Ui_MainWindow(object):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(589, 542)
         MainWindow.setAutoFillBackground(False)
-        MainWindow.setStyleSheet("background-color: rgb(255, 227, 221)\n"
-"")
-
+        MainWindow.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"border-color: rgb(0, 170, 255);")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout_3 = QtWidgets.QGridLayout(self.centralwidget)
         self.gridLayout_3.setObjectName("gridLayout_3")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setStyleSheet("text-decoration: underline;\n"
+"color: rgb(0, 0, 0);\n"
+"font: 14pt \"Ubuntu Condensed\";")
         self.label_2.setAlignment(QtCore.Qt.AlignCenter)
         self.label_2.setObjectName("label_2")
         self.gridLayout_3.addWidget(self.label_2, 2, 2, 1, 1)
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setStyleSheet("font: 57 12pt \"Ubuntu\";")
         self.label_3.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
         self.label_3.setObjectName("label_3")
         self.gridLayout_3.addWidget(self.label_3, 0, 0, 1, 1)
@@ -36,32 +39,44 @@ class Ui_MainWindow(object):
         self.lineEdit.setObjectName("lineEdit")
         self.gridLayout_3.addWidget(self.lineEdit, 1, 1, 1, 1)
         self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setStyleSheet("font: 57 12pt \"Ubuntu\";")
         self.label_4.setObjectName("label_4")
         self.gridLayout_3.addWidget(self.label_4, 1, 0, 1, 1)
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setMaximumSize(QtCore.QSize(100, 16777215))
+        self.pushButton.setStyleSheet("background-color: rgb(0, 170, 255);\n"
+"color: rgb(255, 255, 255);\n"
+"font: 57 12pt \"Ubuntu\";")
+        self.pushButton.setAutoDefault(True)
+        self.pushButton.setDefault(True)
+        self.pushButton.setFlat(False)
         self.pushButton.setObjectName("pushButton")
         self.gridLayout_3.addWidget(self.pushButton, 0, 1, 1, 1)
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setSizeIncrement(QtCore.QSize(200, 200))
+        self.label.setStyleSheet("text-decoration: underline;\n"
+"color: rgb(0, 0, 0);\n"
+"font: 14pt \"Ubuntu Condensed\";")
         self.label.setLineWidth(1)
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
         self.gridLayout_3.addWidget(self.label, 2, 0, 1, 1)
         self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
-        self.progressBar.setStyleSheet("selection-background-color: rgb(46, 52, 54);")
+        self.progressBar.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.progressBar.setStyleSheet("selection-background-color: rgb(0, 170, 255);\n"
+"border-color: rgb(0, 0, 0);")
         self.progressBar.setProperty("value", 0)
+        self.progressBar.setAlignment(QtCore.Qt.AlignCenter)
         self.progressBar.setObjectName("progressBar")
-        self.gridLayout_3.addWidget(self.progressBar, 3, 0, 1, 3)
+        self.gridLayout_3.addWidget(self.progressBar, 4, 0, 1, 3)
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_2.setMaximumSize(QtCore.QSize(16777215, 40))
+        self.pushButton_2.setStyleSheet("background-color: rgb(0, 170, 255);\n"
+"color: rgb(255, 255, 255);\n"
+"font: 57 12pt \"Ubuntu\";")
         self.pushButton_2.setObjectName("pushButton_2")
         self.gridLayout_3.addWidget(self.pushButton_2, 5, 1, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 589, 20))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
@@ -71,13 +86,14 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label_2.setText(_translate("MainWindow", "الصوره الفوريرية"))
-        self.label_3.setText(_translate("MainWindow", "Input an 128*128 image"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Task-1"))
+        self.label_2.setText(_translate("MainWindow", "Fourier Image"))
+        self.label_3.setText(_translate("MainWindow", "Input an 520*520 image"))
         self.lineEdit.setText(_translate("MainWindow", "1"))
         self.lineEdit.setPlaceholderText(_translate("MainWindow", "ex: 520*520"))
         self.label_4.setText(_translate("MainWindow", "Required sequence"))
         self.pushButton.setText(_translate("MainWindow", "Browse"))
-        self.label.setText(_translate("MainWindow", "الصورة"))
-        self.pushButton_2.setText(_translate("MainWindow", "Restart"))
+        self.label.setText(_translate("MainWindow", "Normal Image"))
+        self.progressBar.setFormat(_translate("MainWindow", "%p%"))
+        self.pushButton_2.setText(_translate("MainWindow", "Start"))
 
